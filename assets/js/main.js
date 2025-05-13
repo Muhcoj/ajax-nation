c12297b49ff998a67e49de6c96d01f16da07d3bb
Muhcoj <jochum.de.bueger@gmail.com>
2025-05-13 20:59:14 +0100

    made changes to the responsive men

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const navToggle = document.querySelector('.nav-toggle');
-    const navLinks = document.querySelector('.nav-links');
-  
-    navToggle.addEventListener('click', () => {
-      navLinks.classList.toggle('active');
-    });
-  });
+  const navToggle = document.querySelector('.nav-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  if (navToggle && navLinks) { // Ensure both elements exist
+      navToggle.addEventListener('click', () => {
+          navLinks.classList.toggle('active');
+      });
+  }
+});
   
